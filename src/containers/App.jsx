b4d6f26b2f2600fcbd0ae2063d9fc03c77538491
Oleksandr Matviichuk <sashko1921@gmail.com>
2018-03-03 17:03:51 +0100

    Fixed path to notRequireAuth component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import requireAuth from './hoc/RequireAuth';
-import requireNotAuth from './hoc/RequireNotAuth';
+import requireAuth from './hoc/RequireAuth'
+import requireNotAuth from './hoc/NotRequireAuth'
 
 import Header from './Header';
 import Home from './Home';
